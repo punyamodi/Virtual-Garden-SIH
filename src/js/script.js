fe5c24444875028449ebc8c915da1d41a131bcb3
AishSoni <aishsoni15@gmail.com>
2024-09-05 20:42:51 +0530

    refactored code

--- src/js/script.js
+++ src/js/script.js
@@ -1,109 +1,51 @@
-import * as tjs from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import * as dat from 'dat.gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-//3js Setup / Boilerplate code
-const renderer = new tjs.WebGLRenderer();
+// 3js Setup / Boilerplate code
+// Basic Parameters
+let renderer, camera, scene, orbit;
 
-renderer.shadowMap.enabled = true;
+init();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function init(){
+    renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-const scene = new tjs.Scene();
-const camera = new tjs.PerspectiveCamera(
-    90,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        90,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
 
-const orbit = new OrbitControls(camera, renderer.domElement);
+    // IBL Setup (To be implemented)
 
-//Setting up the basic scene
-const axes = new tjs.AxesHelper(5);
-scene.add(axes);
+    orbit = new OrbitControls(camera, renderer.domElement);
+    camera.position.set(0, 2, 6);
+    orbit.update();
+    window.addEventListener('resize', onWindowResize);
+}
 
-camera.position.set(0, 2, 6);
-orbit.update();
-
-//Setting up various 3D objects present in the scene.
-//Lights
-const ambi = new tjs.AmbientLight(0xFFDF22);
-scene.add(ambi);
-
-const lamp = new tjs.DirectionalLight(0xFFFFFF, 0.8);
-scene.add(lamp);
-lamp.position.set(0, 6, 0);
-// lamp.shadow.camera.bottom(-12);
-
-const lampHelper = new tjs.DirectionalLightHelper(lamp);
-scene.add(lampHelper);
-
-const spotLight = new tjs.SpotLight(0xFFFFFF);
-scene.add(spotLight);
-spotLight.position.set(-100, 100, 0);
-spotLight.castShadow = true;
-
-//Meshes
-const boxgeo = new tjs.BoxGeometry();
-const boxmat = new tjs.MeshBasicMaterial({color: 0x00FF00});
-const box = new tjs.Mesh(boxgeo, boxmat);
-scene.add(box);
-box.position.set(-4, 0, 0);
-
-const planegeo = new tjs.PlaneGeometry(20, 20);
-const planemat = new tjs.MeshStandardMaterial({
-    color: 0x000080,
-    side: tjs.DoubleSide
-});
-const plane = new tjs.Mesh(planegeo, planemat);
-scene.add(plane);
-plane.receiveShadow = true;
-plane.rotation.x = -0.5 * Math.PI;
-
-const spheregeo = new tjs.SphereGeometry(3, 50, 50);
-const spheremat = new tjs.MeshStandardMaterial({
-    color: 0x252500
-});
-const sphere = new tjs.Mesh(spheregeo, spheremat);
-scene.add(sphere);
-sphere.castShadow = true;
-
-const grid = new tjs.GridHelper(20);
-scene.add(grid);
-
-//Setting up the GUI and it's various options.
-const gui = new dat.GUI();
-let step = 0;
-
-const options = {
-    sphereColor: '#252500',
-    boxColor: '#000080',
-    animationSpeed: '0.01'
-};
-
-gui.addColor(options, 'sphereColor').onChange(function(e){
-    sphere.material.color.set(e);
-});
-
-gui.addColor(options, 'boxColor').onChange(function(e){
-    box.material.color.set(e);
-});
-
-gui.add(options, 'animationSpeed', 0, 0.2);
-
-//Defining the animation loop.
-function animate(time){
-    sphere.rotation.x+= 0.005;
-    sphere.rotation.y+= 0.005;
-    sphere.rotation.z+= 0.005;
-
-    step+= options.animationSpeed;
-    sphere.position.y = 10 * Math.abs(Math.sin(step));
-
+// Rendering Setup
+function render(){
     renderer.render(scene, camera);
 }
 
-//Finally, rendering the scene after adding all elements to it.
+// Defining the animation loop.
+function animate(time){
+    render();
+}
+
+// Finally, rendering the scene after adding all elements to it.
 renderer.setAnimationLoop(animate);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
