902fe55eec2424e1a8844dd09d642ed91340347d
AishSoni <aishsoni15@gmail.com>
2024-09-14 11:48:17 +0530

    Added FPP Controls, Physics, Fog

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import * as tjs from 'three';
 import { GUI } from 'lil-gui';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { FirstPersonControls } from 'three/examples/jsm/Addons.js';
 import { RGBELoader } from 'three/examples/jsm/Addons.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import * as cannon from 'cannon-es';
 
 //URL Setup
 const hdri = new URL('./public/hdri/kloppenheim_02_4k.hdr', import.meta.url);
@@ -21,15 +22,17 @@ renderer.outputEncoding = tjs.sRGBEncoding;
 //Camera + Control Setup
 //Near & Far Values here determine the starting & ending points of view distance, heavily affects performance.
 const camera = new tjs.PerspectiveCamera(
-    90,
+    45,
     window.innerWidth / window.innerHeight,
     0.1,
-    100
+    300
 );
 
-const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(6, 8, 14);
-orbit.update();
+const controls = new FirstPersonControls(camera, renderer.domElement);
+controls.movementSpeed = 12;
+controls.lookSpeed = 0.08;
+
+camera.position.set(8, 5, 3);
 
 //Loading Screen Setup
 const loadingManager = new tjs.LoadingManager();
@@ -56,13 +59,14 @@ loadingManager.onError = function(url){
     console.error(`Error loading: ${url}`);
 }
 
-
 /**
  * Scene Setup
  */
 const scene = new tjs.Scene();
 const gltfLoader= new GLTFLoader(loadingManager);
 const rgbeLoader = new RGBELoader(loadingManager);
+
+scene.fog = new tjs.FogExp2(0xBCC8CC, 0.01); // Squared Exponential Fog
 
 rgbeLoader
     .load(hdri.href, function(texture){
@@ -89,7 +93,7 @@ scene.add(axes);
 const grid = new tjs.GridHelper(20);
 scene.add(grid);
 
-const boxGeo = new tjs.BoxGeometry();
+const boxGeo = new tjs.BoxGeometry(2, 2, 2);
 const boxMat = new tjs.MeshPhysicalMaterial({
     metalness: 1,
     roughness: 0.05
@@ -97,6 +101,38 @@ const boxMat = new tjs.MeshPhysicalMaterial({
 const box = new tjs.Mesh(boxGeo, boxMat);
 scene.add(box);
 box.position.set(8, 8, 3);
+
+const groundGeo = new tjs.PlaneGeometry(200, 200, 10, 10);
+const groundMat = new tjs.MeshPhysicalMaterial({
+    side: tjs.DoubleSide
+});
+const ground = new tjs.Mesh(groundGeo, groundMat);
+scene.add(ground);
+
+/*
+// World / Physics Setup
+*/
+
+const world = new cannon.World({
+    gravity: new cannon.Vec3(0, -9.80665, 0)
+});
+
+const timeStep = 1 / 60;
+
+const groundBody = new cannon.Body({
+    shape: new cannon.Plane(),
+    mass: 10,
+    type: cannon.Body.STATIC
+});
+world.addBody(groundBody);
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+
+const boxBody = new cannon.Body({
+    mass: 2,
+    shape: new cannon.Box(new cannon.Vec3(2, 2, 2)),
+    position: new cannon.Vec3(1, 20, 0)
+});
+world.addBody(boxBody);
 
 //GUI Setup
 const params = {
@@ -115,9 +151,20 @@ function render(){
 }
 
 //Animation Loop
+const clock = new tjs.Clock();
+
 function animate(){
+    world.step(timeStep);
+
+    ground.position.copy(groundBody.position);
+    ground.quaternion.copy(groundBody.quaternion);
+
+    box.position.copy(boxBody.position);
+    box.quaternion.copy(boxBody.quaternion);
+
     render();
     stats.update();
+    controls.update(clock.getDelta());
 }
 
 renderer.setAnimationLoop(animate);
